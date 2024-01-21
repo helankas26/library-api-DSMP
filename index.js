4e95ef3b9434aa1246fc1e5fedd8bf0f72192e7e
Helanka Singhapurage <helankas26@gmail.com>
2024-01-21 10:40:00 +0530

    update index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const {PORT} = require('./config/serverConfig');
+
+//----------------------- Routes import
+// const userRoute = require('./route/UserRoute');
+//-----------------------
+
+const app = express();
+app.use(cors());
+
+/*
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+*/
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+
+        app.listen(PORT, () => {
+            console.log(`server started & running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    });
+
+
+//----------------------- Routes
+// app.use('/api/v1/users', userRoute);
