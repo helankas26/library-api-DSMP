063c9f4c34ccf6b9ecca14a638a2bcc32e897871
Helanka Singhapurage <helankas26@gmail.com>
2024-01-24 04:33:42 +0530

    implement param middleware

--- src/routes/api/ConfigRoute.js
+++ src/routes/api/ConfigRoute.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
 const configController = require('../../controllers/ConfigController');
+const paramMiddleware = require('../../middlewares/ParamMiddleware');
+const Config = require('../../models/ConfigSchema')
 
 const router = express.Router();
+
+router.param('id', async (req, res, next, value) => {
+    await paramMiddleware.verifyId(req, res, next, value, Config);
+});
 
 router.route('/')
     .get(configController.findAllConfigs)
