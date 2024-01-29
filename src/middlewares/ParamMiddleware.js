a72c82fb883ad3193e2e6f1eb16da78d5e8745d1
Helanka Singhapurage <helankas26@gmail.com>
2024-01-29 12:24:55 +0530

    change error handling in param middleware

--- src/middlewares/ParamMiddleware.js
+++ src/middlewares/ParamMiddleware.js
@@ -1,21 +1,17 @@
+const NotFoundError = require("../errors/NotFoundError");
+
 const verifyId = async (req, res, next, id, Model) => {
     try {
         let document = await Model.findById(id);
 
         if (!document) {
-            return res.status(404).json({
-                status: "fail",
-                message: `Document with ID ${id} not found for ${Model.modelName}`
-            });
+            throw new NotFoundError(`Document with ID ${id} not found for ${Model.modelName}`);
         }
 
         req.document = document;
         next();
     } catch (err) {
-        return res.status(500).json({
-            status: "error",
-            message: err.message
-        });
+        return next(err);
     }
 }
 
