218167273e03278ef1186f7807726959f8009bb4
Helanka Singhapurage <helankas26@gmail.com>
2024-01-30 19:04:02 +0530

    attach jwt token to cookie

--- src/utils/SendResponseUtil.js
+++ src/utils/SendResponseUtil.js
@@ -1,6 +1,20 @@
 const {signToken} = require("./SignTokenGenerateUtil");
+const {LOGIN_EXPIRES, NODE_ENV} = require('../../config/serverConfig');
+
 const sendResponseWithToken = async (res, statusCode, user) => {
     const token = await signToken(user._id);
+
+    const options = {
+        maxAge: LOGIN_EXPIRES,
+        httpOnly: true
+    };
+
+    if (NODE_ENV === 'production')
+        options.secure = true
+
+    res.cookie('jwt', token, options);
+
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
