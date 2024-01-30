c025fed057f0120858695620d6b06402e93fe1ca
Helanka Singhapurage <helankas26@gmail.com>
2024-01-30 17:37:50 +0530

    update server config

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.SERVER_PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    LOGIN_EXPIRES: process.env.LOGIN_EXPIRES || '24h',
+    LOGIN_EXPIRES: process.env.LOGIN_EXPIRES || 86400000,
     EMAIL_SERVICE: process.env.EMAIL_SERVICE || 'gmail',
     EMAIL_USER: process.env.EMAIL_USER || 'helankas26@gmail.com'
 };
