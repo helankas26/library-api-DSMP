9eea3d450368f0f730f26138a9c19747532700ab
Helanka Singhapurage <helankas26@gmail.com>
2024-01-30 17:14:51 +0530

    update sign token util

--- src/utils/SignTokenGenerateUtil.js
+++ src/utils/SignTokenGenerateUtil.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const {LOGIN_EXPIRES} = require('../../config/serverConfig');
 
 const signToken = async (id) => {
-    return jwt.sign({id}, process.env.SECRET_KEY, {
+    return await jwt.sign({id}, process.env.SECRET_KEY, {
         expiresIn: LOGIN_EXPIRES
     });
 }
