d2d0ff698d40addf93532231325b6834d025cf2d
Helanka Singhapurage <helankas26@gmail.com>
2024-06-22 01:40:03 +0530

    refactor subscription create and delete add transaction

--- src/repositories/SubscriptionRepository.js
+++ src/repositories/SubscriptionRepository.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const Subscription = require('../models/SubscriptionSchema');
 const Config = require("../models/ConfigSchema");
 const Profile = require("../models/ProfileSchema");
@@ -11,16 +13,20 @@ const findAllSubscriptions = async () => {
 }
 
 const createSubscription = async (req) => {
+    const session = await mongoose.startSession();
+
     try {
+        session.startTransaction();
+
         const subscriptionData = req.body;
 
         const subscription = new Subscription({
             fee: subscriptionData.fee ? subscriptionData.fee : await (async () => {
-                const config = await Config.findOne();
+                const config = await Config.findOne().session(session);
                 return config.subscription.fee;
             })(),
             paidFor: await (async () => {
-                const profile = await Profile.findById(subscriptionData.member);
+                const profile = await Profile.findById(subscriptionData.member).session(session);
                 const options = {month: 'long', year: 'numeric'};
                 let paidFor = new Date();
 
@@ -34,9 +40,34 @@ const createSubscription = async (req) => {
             librarian: req.user.profile
         });
 
-        return await subscription.save();
+        const savedSubscription = await subscription.save({session: session});
+        if (!savedSubscription) {
+            throw new Error("Payment unsuccessful. Try again!");
+        }
+
+        try {
+            const profile = await Profile.findById(subscriptionData.member).session(session);
+            if (!profile) {
+                throw new Error("Profile not found. Payment unsuccessful. Try again!");
+            }
+
+            if (profile.paymentStatus > 0) {
+                profile.paymentStatus -= 1;
+                await profile.save({session: session});
+            } else {
+                throw new Error("Payment unsuccessful. Could not pay for up coming month!");
+            }
+        } catch (error) {
+            throw error;
+        }
+
+        await session.commitTransaction();
+        return savedSubscription;
     } catch (error) {
+        await session.abortTransaction();
         throw error;
+    } finally {
+        await session.endSession();
     }
 }
 
@@ -67,10 +98,32 @@ const updateSubscription = async (params, req) => {
 }
 
 const deleteSubscription = async (params) => {
+    const session = await mongoose.startSession();
+
     try {
-        return await Subscription.findByIdAndDelete(params.id);
+        session.startTransaction();
+
+        const payment = await Subscription.findById(params.id).session(session);
+        if (!payment) {
+            throw new Error("Payment not found. Try again!");
+        }
+
+        const profile = await Profile.findById(payment.member).session(session);
+        if (!payment) {
+            throw new Error("Member not found. Try again!");
+        }
+
+        profile.paymentStatus += 1;
+        await profile.save({session: session});
+        const subscription = await Subscription.findByIdAndDelete(params.id).session(session);
+
+        await session.commitTransaction();
+        return subscription;
     } catch (error) {
+        await session.abortTransaction();
         throw error;
+    } finally {
+        await session.endSession();
     }
 }
 
